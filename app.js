21c2e22cf6900d6594f13609af3f24ab71703e8b
Philippe-SS <lipe.philippe64@gmail.com>
2021-09-23 18:06:24 -0300

    changed the message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello everyone!'));
+app.get('/', (req, res) => res.send('Uhm? vou descer, vou comer vou estudar, paz de deus, vou estudar, ou amor fala!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
